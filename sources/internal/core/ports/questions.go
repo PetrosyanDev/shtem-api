99cfee22ed5b1b0800c71160b07deb71543f8001
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2023-12-11 22:03:07 +0400

    work.

--- sources/internal/core/ports/questions.go
+++ sources/internal/core/ports/questions.go
@@ -5,7 +5,7 @@ import "shtem-api/sources/internal/core/domain"
 type QuestionsService interface {
 	Create(question *domain.Question) domain.Error
 	Update(question *domain.Question) domain.Error
-	Delete(question *domain.Question) domain.Error
+	Delete(id int) domain.Error
 	FindByShtem(question *domain.Question) (*domain.Question, domain.Error)
 	FindByID(id int) (*domain.Question, domain.Error)
 }
