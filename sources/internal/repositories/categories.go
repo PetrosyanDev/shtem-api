1bccb3c810f8c5262c74406a32df0e386b04c3dd
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2024-02-01 20:49:14 +0400

    work.

---
+++ sources/internal/repositories/categories.go
@@ -0,0 +1,27 @@
+package repositories
+
+import (
+	"shtem-api/sources/internal/core/domain"
+	postgresrepository "shtem-api/sources/internal/repositories/postgres"
+)
+
+type categoriesRepository struct {
+	db postgresrepository.CategoriesDB
+}
+
+func (p *categoriesRepository) GetCategories() ([]*domain.Category, domain.Error) {
+	return p.db.GetCategories()
+}
+func (p *categoriesRepository) GetCategoryByLinkName(c_link_name string) (*domain.Category, domain.Error) {
+	return p.db.GetCategoryByLinkName(c_link_name)
+}
+func (p *categoriesRepository) GetCategoriesWithShtems() (domain.Categories, domain.Error) {
+	return p.db.GetCategoriesWithShtems()
+}
+func (p *categoriesRepository) GetShtemsByCategoryLinkName(c_linkName string) ([]*domain.Shtemaran, domain.Error) {
+	return p.db.GetShtemsByCategoryLinkName(c_linkName)
+}
+
+func NewCategoriesRepository(db postgresrepository.CategoriesDB) *categoriesRepository {
+	return &categoriesRepository{db}
+}
--- sources/internal/repositories/common.go
+++ sources/internal/repositories/common.go
@@ -6,9 +6,22 @@ import "shtem-api/sources/internal/core/domain"
 type QuestionsRepository interface {
 	Create(question *domain.Question) domain.Error
 	Update(question *domain.Question) domain.Error
-	Delete(id int) domain.Error
+	Delete(id int64) domain.Error
 	FindBajin(question *domain.Question) ([]*domain.Question, domain.Error)
-	FindQuestionByNumber(question *domain.Question) (*domain.Question, domain.Error)
-	FindByID(id int) (*domain.Question, domain.Error)
-	GetShtemNames() ([]string, domain.Error)
+	FindQuestion(question *domain.Question) (*domain.Question, domain.Error)
+	FindByID(id int64) (*domain.Question, domain.Error)
+}
+
+type ShtemsRepository interface {
+	GetShtems() ([]*domain.Shtemaran, domain.Error)
+	GetShtemsByCategoryId(c_id int64) ([]*domain.Shtemaran, domain.Error)
+	GetShtemLinkNames() ([]string, domain.Error)
+	GetShtemByLinkName(name string) (*domain.Shtemaran, domain.Error)
+}
+
+type CategoriesRepository interface {
+	GetCategories() ([]*domain.Category, domain.Error)
+	GetCategoryByLinkName(c_link_name string) (*domain.Category, domain.Error)
+	GetCategoriesWithShtems() (domain.Categories, domain.Error)
+	GetShtemsByCategoryLinkName(c_linkName string) ([]*domain.Shtemaran, domain.Error)
 }
