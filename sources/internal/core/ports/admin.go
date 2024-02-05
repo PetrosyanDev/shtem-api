c177a7074670f15aabe20b1ca4621bbb677d209a
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2024-02-05 19:07:05 +0400

    work.

--- sources/internal/core/ports/admin.go
+++ sources/internal/core/ports/admin.go
@@ -1,8 +1,18 @@
 package ports
 
-import "github.com/gin-gonic/gin"
+import (
+	"shtem-api/sources/internal/core/domain"
+
+	"github.com/gin-gonic/gin"
+)
 
 type AdminHandler interface {
 	GenerateToken() gin.HandlerFunc
 	ValidateToken() gin.HandlerFunc
+}
+
+type AdminService interface {
+	Create(username, password, token string) (*domain.Admin, domain.Error)
+	Update(adm *domain.Admin) domain.Error
+	Delete(id int64) domain.Error
 }
---
+++ sources/internal/core/services/admin.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"shtem-api/sources/internal/core/domain"
+	"shtem-api/sources/internal/repositories"
+)
+
+type adminService struct {
+	adminRepository repositories.AdminRepository
+}
+
+func (q *adminService) Create(username, password, token string) (*domain.Admin, domain.Error) {
+	return q.adminRepository.Create(username, password, token)
+}
+
+func (q *adminService) Update(adm *domain.Admin) domain.Error {
+	return q.adminRepository.Update(adm)
+}
+
+func (q *adminService) Delete(id int64) domain.Error {
+	return q.adminRepository.Delete(id)
+}
+
+func NewAdminService(adminRepository repositories.AdminRepository) *adminService {
+	return &adminService{adminRepository}
+}
